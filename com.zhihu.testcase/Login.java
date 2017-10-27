68fdff519ec41e5f0d88c2291f53b43fea3bee47
mff201302166 <253742058@qq.com>
2017-10-27 10:17:35 +0800

    Add files via upload

---
+++ com.zhihu.testcase/Login.java
@@ -0,0 +1,33 @@
+package com.zhihu.testcase;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.zhihu.common.*;
+import org.testng.annotations.Parameters;
+
+public class Login extends StartAppium{
+	@Parameters({"username","password"})
+    @Test
+  public void login(String username,String password) throws Exception {
+		try{
+			  WebElement loginIcon=IconLocate.findIcon("ID", "com.zhihu.android:id/login_btn", false, 0);
+			  IconOperate.clickIcon(loginIcon);
+			  WebElement inputUserIcon=IconLocate.findIcon("ID", "com.zhihu.android:id/username", false, 0);
+			  IconOperate.sendKeys(inputUserIcon, username);    
+			  WebElement inputPassword=IconLocate.findIcon("ID", "com.zhihu.android:id/password", false, 0);
+			  IconOperate.sendKeys(inputPassword, password);
+		      WebElement NxtBtn=IconLocate.findIcon("ID", "com.zhihu.android:id/btn_progress", false, 0);   		
+		      Assert.assertEquals(NxtBtn.isEnabled(), true); 
+		      IconOperate.clickIcon(NxtBtn);
+		      Thread.sleep(5000);
+			
+		}
+		catch(Exception e){
+			ScreenShot.getScreenShot(driver, "µÇÂ¼Òì³£");			
+			
+		}
+	  
+      
+  }
+}
